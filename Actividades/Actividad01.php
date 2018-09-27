8c3e516c2346d5fffbcda75e38fc91150fca9ded
WINMAC17\2daw <sergio_311096@hotmail.es>
2018-09-27 13:40:16 +0200

    subir actividad01

--- Actividades/Actividad01.php
+++ Actividades/Actividad01.php
@@ -7,69 +7,10 @@
 <body>
 
 <h1>Actividad 01</h1>
-
-
-<div style="background-color: rgb(0,0,0)">rgb(0,0,0)</div>
-
 <?php
-$w = 0;
-
-do{
-    echo "<div style='background-color: rgb(0,$w,0)'>0,$w,0</div>";
-    ++$w;
-} while ($w <= 255);
-
-$w = 0;
-
-do{
-    echo "<div style='background-color: rgb($w,0,0)'>$w,0,0</div>";
-    ++$w;
-} while ($w <= 255);
-
-$w = 0;
-
-do{
-    echo "<div style='background-color: rgb(0,0,$w)'>0,0,$w</div>";
-    ++$w;
-} while ($w <= 255);
-
-$w = 0;
-$x = 0;
-
-while ($w <= 255 && $w <= 255){
-    echo "<div style='background-color: rgb(0,$w,$x)'>0,$w,$x</div>";
-    ++$w;
-    ++$x;
-}
-
-$w = 0;
-$x = 0;
-
-while ($w <= 255 && $w <= 255){
-    echo "<div style='background-color: rgb($x,$w,0)'>$x,$w,0</div>";
-    ++$w;
-    ++$x;
-}
-
-$w = 0;
-$x = 0;
-
-while ($w <= 255 && $w <= 255){
-    echo "<div style='background-color: rgb($x,0,$w)'>$x,0,$w</div>";
-    ++$w;
-    ++$x;
-}
-
-$w = 0;
-$x = 0;
-$z = 0;
-
-while ($w <= 255 && $w <= 255){
-    echo "<div style='background-color: rgb($x,$z,$w)'>$x,$z,$w</div>";
-    ++$w;
-    ++$x;
-    ++$z;
-}
-?>
+for ($i=0;$i<=255;$i++){
+    echo '<div style="background-color: rgb(0, '.$i.' , 0)">(0, '.$i.',0)</div>';
+    }
+?>    
 </body>
 </html>
