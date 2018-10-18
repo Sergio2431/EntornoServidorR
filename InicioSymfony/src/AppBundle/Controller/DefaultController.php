fbd90624925a31a477354c014313a8987ff066e6
WINMAC17\2daw <sergio_311096@hotmail.es>
2018-10-18 13:38:56 +0200

    subir archivos

--- InicioSymfony/src/AppBundle/Controller/DefaultController.php
+++ InicioSymfony/src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Enity\Producto;
 
 
 class DefaultController extends Controller
@@ -38,5 +39,14 @@ class DefaultController extends Controller
     public function yoAction(Request $request)
     {
         return $this->render('sergio/sergio.html.twig');
+    }
+    /**
+     * @Route("/producto/", name="producto")
+     */
+    public function productoAction(Request $request)
+    {
+        $repository = $this->getDoctrine()->getRepository(Producto::class);
+        $products = $repository->findAll();
+        return $this->render('sergio/productos.html.twig',array(productos));
     }
 }
---
+++ InicioSymfony/src/AppBundle/Entity/Producto.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Producto
+ *
+ * @ORM\Table(name="producto")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ProductoRepository")
+ */
+class Producto
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="text")
+     */
+    private $nombre;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="cantidad", type="integer")
+     */
+    private $cantidad;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Producto
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set cantidad
+     *
+     * @param integer $cantidad
+     *
+     * @return Producto
+     */
+    public function setCantidad($cantidad)
+    {
+        $this->cantidad = $cantidad;
+
+        return $this;
+    }
+
+    /**
+     * Get cantidad
+     *
+     * @return int
+     */
+    public function getCantidad()
+    {
+        return $this->cantidad;
+    }
+}
+
